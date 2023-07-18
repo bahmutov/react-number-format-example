2824b433e10b3898118d20fc26af14f1a30eecea
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-07-18 16:53:05 -0400

    unhappy tests

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -17,3 +17,21 @@ it('enters phone number and dollar amount', () => {
   cy.get('[data-cy=phone]').should('have.value', '+1 (234) 567 8900')
   cy.get('[data-cy=price]').should('have.value', '$18.99')
 })
+
+it('ignores invalid characters in price input', () => {
+  cy.visit('/')
+  cy.get('[data-cy=price]')
+    .clear()
+    .type('-012.345.678')
+    .should('have.value', '$012.34')
+    .blur()
+    .should('have.value', '$12.34')
+})
+
+it('ignores invalid characters in phone input', () => {
+  cy.visit('/')
+  cy.get('[data-cy=phone]')
+    .type('-phone 123/456-7890.1234')
+    // all characters but 10 digit phone are removed
+    .should('have.value', '+1 (123) 456 7890')
+})
