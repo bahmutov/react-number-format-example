10e8257409bb0c70e2cb1ce5e92406adb06cbb6e
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-07-18 17:30:08 -0400

    refactored test

--- cypress/component/onValueChange.cy.jsx
+++ cypress/component/onValueChange.cy.jsx
@@ -1,8 +1,10 @@
 import { NumericFormat } from 'react-number-format'
 import '../../src/App.css'
+// https://github.com/bahmutov/cypress-map
+import 'cypress-map'
 
 describe('onValueChange.cy.jsx', () => {
-  it('playground', () => {
+  it('calls onValueChange', () => {
     cy.mount(
       <NumericFormat
         value={1234}
@@ -27,38 +29,30 @@ describe('onValueChange.cy.jsx', () => {
     // check each call
     cy.get('@onValueChange').should('have.property', 'callCount', 5)
     cy.get('@onValueChange')
-      .its('firstCall.args.0')
-      .should('deep.equal', { formattedValue: '$5', value: '5', floatValue: 5 })
-    cy.get('@onValueChange').its('secondCall.args.0').should('deep.equal', {
-      formattedValue: '$50',
-      value: '50',
-      floatValue: 50,
-    })
-    cy.get('@onValueChange').its('thirdCall.args.0').should('deep.equal', {
-      formattedValue: '$50.',
-      value: '50.',
-      floatValue: 50,
-    })
-    cy.get('@onValueChange').its('thirdCall.args.0').should('deep.equal', {
-      formattedValue: '$50.',
-      value: '50.',
-      floatValue: 50,
-    })
-    cy.get('@onValueChange')
-      .invoke('getCall', 3)
-      .its('args.0')
-      .should('deep.equal', {
-        formattedValue: '$50.9',
-        value: '50.9',
-        floatValue: 50.9,
-      })
-    cy.get('@onValueChange')
-      .invoke('getCall', 4)
-      .its('args.0')
-      .should('deep.equal', {
-        formattedValue: '$50.99',
-        value: '50.99',
-        floatValue: 50.99,
-      })
+      .invoke('getCalls')
+      .map('args.0')
+      .should('deep.equal', [
+        { formattedValue: '$5', value: '5', floatValue: 5 },
+        {
+          formattedValue: '$50',
+          value: '50',
+          floatValue: 50,
+        },
+        {
+          formattedValue: '$50.',
+          value: '50.',
+          floatValue: 50,
+        },
+        {
+          formattedValue: '$50.9',
+          value: '50.9',
+          floatValue: 50.9,
+        },
+        {
+          formattedValue: '$50.99',
+          value: '50.99',
+          floatValue: 50.99,
+        },
+      ])
   })
 })
